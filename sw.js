5a42bb89f3327444bf5df394ac1bbb3935c3b4ec
waitube <waitube2@gmail.com>
2026-09-10 21:53:53 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -21,8 +21,8 @@ const ASSETS = [
   './universities-data.js',
   './real-susi-placement-data.js',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
+  './icon-192.png',
+  './icon-512.png',
 ];
 
 self.addEventListener('install', (event) => {
